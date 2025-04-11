2b14634a21cb92e3577f1e3b963fd8f81da41c9c
wwsshh0430 <wwsshh0430@naver.com>
2025-04-11 17:13:37 +0900

    2025-04-11 17:13

--- src/main/webapp/WEB-INF/views/record/batterStat.sql
+++ src/main/webapp/WEB-INF/views/record/batterStat.sql
@@ -1,31 +1,31 @@
 CREATE TABLE player_stats (
-    id INT AUTO_INCREMENT PRIMARY KEY, /*mid*/
-    player VARCHAR(10),	/*선수명*/
-    team_logo VARCHAR(20),	/**/
-    position VARCHAR(5),
+    id INT AUTO_INCREMENT PRIMARY KEY, /*mid(넣긴하는데 프론트에 보이진않게해야함)*/
     rank INT,/*순위(뺴야함)*/
-    year INT,/*년도*/
+    player VARCHAR(10),	/*선수명*/
+    team_logo VARCHAR(20),	/*팀로고*/
+    position VARCHAR(5), /*선수포지션*/
     war INT,/*대체선수대비 승려기여도(WAR)*/
+    year INT,/*년도*/
     games INT, /*경기수*/
-    plate_appearances INT, /**/
-    at_bats INT,
-    runs INT,
-    hits INT,
-    doubles INT,
-    triples INT,
-    total_bases INT,	
-    rbi INT,
-    home_runs INT,
-    stolen_bases INT,
-    caught_stealing INT,
-    bb INT,
-    strikeouts INT,
-    double_plays INT,
-    sac_hits INT,
-    sac_flies INT,
+    tasuk INT, /*타석*/
+    at_bats INT, /*타수*/
+    runs INT, /*득점*/
+    hits INT, /*안타*/
+    doubles INT, /*2루타*/
+    triples INT, /*3루타*/
+    total_bases INT,	/*루타 : (안타-2루타-3루타-4루타)+2루타x2+3루타x3+홈런x4*/
+    rbi INT,/*타점*/
+    home_runs INT, /*홈런*/
+    stolen_bases INT, /*도루*/
+    stolen_bases_fail INT, /*도루실패*/
+    bb INT, /*사사구*/
+    strikeouts INT, /*삼진*/
+    double_plays INT, /*병살*/
+    sac_hits INT, /*희생타*/
+    sac_flies INT, /*희생플라이*/
 
-    avg DOUBLE(5,3),
-    obp DOUBLE(5,3),
-    slg DOUBLE(5,3),
-    ops DOUBLE(5,3)
+    avg DOUBLE(5,3), /*타율*/
+    obp DOUBLE(5,3), /*출루율*/
+    slg DOUBLE(5,3), /*장타율*/
+    ops DOUBLE(5,3) /*ops(출루율+장타율)*/
 );
