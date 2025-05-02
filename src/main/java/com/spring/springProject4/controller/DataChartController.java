b5c94b380ad135ce48f6da258d169b7f62598a31
wsh0430 <wwsshh0430@naver.com>
2025-05-02 12:26:16 +0900

    Update DataChartController.java

--- src/main/java/com/spring/springProject4/controller/DataChartController.java
+++ src/main/java/com/spring/springProject4/controller/DataChartController.java
@@ -1,9 +1,6 @@
 package com.spring.springProject4.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,8 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.spring.springProject4.dto.PitcherRecordDto;
+import com.spring.springProject4.dto.PlayerRecordDto;
+import com.spring.springProject4.dto.TeamPitcherRecordDto;
 import com.spring.springProject4.dto.TeamPlayerRecordDto;
-import com.spring.springProject4.service.DataChartService;
 import com.spring.springProject4.service.PitcherRecordService;
 import com.spring.springProject4.service.PlayerRecordService;
 import com.spring.springProject4.service.TeamPitcherRecordService;
@@ -22,9 +21,6 @@ import com.spring.springProject4.service.TeamPlayerRecordService;
 @Controller
 @RequestMapping("/dataChart")
 public class DataChartController {
-
-  @Autowired
-  private DataChartService dataChartService;
 
   @Autowired
   private PlayerRecordService playerRecordService;
@@ -37,13 +33,15 @@ public class DataChartController {
 
   @Autowired
   private TeamPitcherRecordService teamPitcherRecordService;
-	//JSP 호출용
-	@GetMapping("/dataChartMain")
+  
+	//JSP 호출(팀타격)
+	@GetMapping("/teamPlayerAverage")
 	public String showChartPage() {
-	   return "dataChart/dataChartMain";  // 너의 jsp 위치에 맞춰서 경로 수정
+	   return "dataChart/teamPlayerAverage"; 
 	}
 	
-  @GetMapping("/teamPlayerAverage")
+	//차트(팀타격)
+  @GetMapping("/teamPlayerAverage2")
   @ResponseBody
   public List<TeamPlayerRecordDto> getTeamPlayerAverage(
       @RequestParam("field") String field,
@@ -51,4 +49,58 @@ public class DataChartController {
       @RequestParam("endYear") int endYear) {
       return teamPlayerRecordService.getAverageByYear(field, startYear, endYear);
   }
+  
+	//JSP 호출(팀투구)
+	@GetMapping("/teamPitcherAverage")
+	public String showTeamPitcherChartPage() {
+	   return "dataChart/teamPitcherAverage"; 
+	}
+  
+	//차트(팀투구)
+  @RequestMapping("/teamPitcherAverage2")
+  @ResponseBody
+  public List<TeamPitcherRecordDto> getPitcherAverageByYear(
+          @RequestParam("field") String field,
+          @RequestParam("startYear") int startYear,
+          @RequestParam("endYear") int endYear) {
+      return teamPitcherRecordService.getPitcherAverageByYear(field, startYear, endYear);
+  }
+  
+	//JSP 호출(개인타자)
+	@GetMapping("/playerInfo")
+	public String showChartPage2() {
+	   return "dataChart/playerInfo"; 
+	}
+	
+	//차트(개인타자)
+  @GetMapping("/playerInfo2")
+  @ResponseBody
+  public List<PlayerRecordDto> getPlayerAverage(
+      @RequestParam("playerName") String playerName,
+      @RequestParam("position") String position,
+      @RequestParam("field") String field,
+      @RequestParam("startYear") int startYear,
+      @RequestParam("endYear") int endYear
+      ) {
+      return playerRecordService.getAverageCompaerByYear(playerName, position, field, startYear, endYear);
+  }
+  
+	//JSP 호출(개인투수)
+	@GetMapping("/comparePitcher")
+	public String showChartPage3() {
+	   return "dataChart/comparePitcher"; 
+	}
+  
+  //차트(개인투수)
+  @GetMapping("/comparePitchers2")
+  @ResponseBody
+  public List<PitcherRecordDto> compareTwoPitchers(
+          @RequestParam String player1,
+          @RequestParam String player2,
+          @RequestParam int startYear,
+          @RequestParam int endYear,
+          @RequestParam String field) {
+      return pitcherRecordService.compareTwoPitchersByYear(player1, player2, startYear, endYear, field);
+  }
+
 }
